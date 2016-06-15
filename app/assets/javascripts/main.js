1b0f23677beffea4c38eea63765f93fd109e87c4
doloreel <jarimado@gmail.com>
2016-06-15 15:50:41 +1000

    stop adding stuff to db on load

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
     Backbone.history.start();
     app.flights.fetch();
 
-    var sydtony = new app.Flight();
-    sydtony.set({
-        origin          : 'SYD',
-        destination     : 'NY',
-    })
-    sydtony.save();
-    app.flights.add(sydtony);
+    // var sydtony = new app.Flight();
+    // sydtony.set({
+    //     origin          : 'SYD',
+    //     destination     : 'NY',
+    // })
+    // sydtony.save();
+    // app.flights.add(sydtony);
 })
