e072fe59d0aed7b50fa99df21b6659dca958f321
doloreel <jarimado@gmail.com>
2016-06-15 15:08:20 +1000

    atteptint to save to database

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,5 +3,17 @@ app.flights = new app.Flights();
 $(document).ready(function() {
     app.router = new app.AppRouter();
     Backbone.history.start();
-    app.flights.fetch(); 
+    app.flights.fetch();
+
+    var sydtony = new app.Flight();
+    sydtony.set({
+        flight_id       : '',
+        plane_id        : '',
+        departure_date  : '',
+        origin          : '',
+        destination     : '',
+        created_at      : '',
+        updated_at      : ''
+    })
+    sydtony.save();
 })
