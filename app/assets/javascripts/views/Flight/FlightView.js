cd447fd5b6269449694e2ab7bef575450f030b2a
doloreel <jarimado@gmail.com>
2016-06-17 10:33:42 +1000

    added logs to poll

--- app/assets/javascripts/views/Flight/FlightView.js
+++ app/assets/javascripts/views/Flight/FlightView.js
@@ -4,9 +4,9 @@ var app = app || {};
 app.FlightView = Backbone.View.extend({
     el: '#container',
     render: function (id) {
+
         var currentFlight = app.flights.get(id).toJSON();
         var planeModel = app.airplanes.get(currentFlight.airplane_id).toJSON();
-
         var flightViewTemplate = $('#FlightViewTemplate').html();
         this.$el.html(flightViewTemplate);
         var flightDetailsView = new app.FlightDetailsView();
